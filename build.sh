916951c283b0e4f4fedacfa8ea101b0010556042
devendrn <devman2857@gmail.com>
2023-06-29 17:48:05 +0530

    Update build script

--- build.sh
+++ build.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-MBT_JAR=env/jar/MaterialBinTool-*.jar
+MBT_JAR=env/jar/MaterialBinTool-0.8.*.jar
 SHADERC=env/bin/shaderc
 
-MBT_THREADS=2
-MBT_ARGS="--compile --shaderc $SHADERC --include include/ --threads $MBT_THREADS"
+MBT_ARGS="--compile --shaderc $SHADERC --include include/ --threads 2"
 
 DATA_DIR=data
 BUILD_DIR=build
 MATERIAL_DIR=materials
 
-#TARGETS="android windows ios"
+# android windows ios"
 TARGETS=""
+
+MATERIALS=""
 
 ARGS=("$@")
 for t in "${ARGS[@]}"; do
   if [ "$t" == "android" ] || [ "$t" == "windows" ] || [ "$t" == "ios" ] ; then
     TARGETS+="$t "
+  else
+    MATERIALS+="$MATERIAL_DIR/$t "
   fi
   shift
 done
@@ -26,12 +29,19 @@ do
   echo "----------------------------------------------"
   if [ -d "$DATA_DIR/$p" ]; then
     echo "Building materials: target=$p"
-    for s in $MATERIAL_DIR/*
+
+    if [ -z $MATERIALS ]; then
+      # all materials
+      MATERIALS=$MATERIAL_DIR/*
+    fi
+
+    for s in $MATERIALS
     do
       echo
       echo " - $s"
       java -jar $MBT_JAR $MBT_ARGS --output $BUILD_DIR/$p --data $DATA_DIR/$p/${s##*/} $s
     done
+
   else
     echo "Build aborted for $p: $DATA_DIR/$p not found"
   fi
