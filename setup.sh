683dcae47cedf70be9189d5416dc33b328c5902f
devendrn <91605478+devendrn@users.noreply.github.com>
2024-01-06 11:30:35 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,18 +9,18 @@ DATA_DIR=data/$DATA_VER
 MBT_JAR_URL="https://github.com/ddf8196/MaterialBinTool/releases/download/v$MBT_VERSION/MaterialBinTool-$MBT_VERSION-all.jar"
 M_DATA_URL="https://cdn.discordapp.com/attachments/1137039470441550004/1178732085628915803/materials-data-$DATA_VER-few.zip"
 
-SHADERC_URL=
 CPU_ARCH=$(uname -m)
 if [ $CPU_ARCH == "x86_64" ]; then
-  SHADERC_URL="https://cdn.discordapp.com/attachments/1137039470441550004/1139817534498021396/shaderc.x86_64"
+  CPU_ARCH="x86_64"
 elif [ $CPU_ARCH == "aarch64" ]; then
-  SHADERC_URL="https://cdn.discordapp.com/attachments/1137039470441550004/1139817534149902356/shaderc.arm64"
+  CPU_ARCH="arm64"
 elif [ $CPU_ARCH == "armv7l" ] || [ $CPU_ARCH == "armv8l" ]; then
-  SHADERC_URL="https://cdn.discordapp.com/attachments/1137039470441550004/1139817533805953145/shaderc.arm32"
+  CPU_ARCH="arm32"
 else
   echo "Error: No shaderc binary found for $CPU_ARCH"
   exit 1;
 fi
+SHADERC_URL="https://github.com/devendrn/RenderDragonSourceCodeInv/releases/download/v1/shaderc.$CPU_ARCH"
 
 if [ "$1" == "-f" ]; then
   # clean
