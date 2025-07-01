48ceb507b89189a237c5ce092ad8d584d042d5a2
Fzul <162413089+faizul726@users.noreply.github.com>
2025-07-01 09:38:32 +0600

    build.sh: add python check

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# You may need to change python3 to python for your system.
+
+command -v python3 >/dev/null 2>&1 || {
+    echo "Python not found." >&2
+    exit 1
+}
+
 LD_LIBRARY_PATH=./tool/lib python3 tool $@
