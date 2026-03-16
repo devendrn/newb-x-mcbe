e4ed9bb033e2bfb4bd734da3d82502f1a92039f5
Ngô Minh Vĩ <148877568+ngmvix2010@users.noreply.github.com>
2026-03-16 19:21:25 +0700

    Implement OS detection and Android support in build.sh
    Add platform detection and handling for Android in build script.

--- build.sh
+++ build.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
+PLATFORM=$(python ./tool/os_detect.py)
+
+case "$PLATFORM" in
+    "android")
+        if [ ! -f "./tool/lib/libc++_shared.so" ]; then
+            echo "./tool/lib/libc++_shared.so not found!"
+            echo "fix lib for android"
+            cp /data/data/com.termux/files/usr/lib/libc++_shared.so ./tool/lib/libc++_shared.so
+        fi
+    ;;
+
+    "other")
+        echo "Error: Operating system '$PLATFORM' is not explicitly supported for setup!" >&2
+        exit 1
+    ;;
+esac
 
 LD_LIBRARY_PATH=./tool/lib python3 tool $@
