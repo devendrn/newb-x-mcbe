d563a670be959f26e0d7acea5e230df8342d5db0
faizul726 <162413089+faizul726@users.noreply.github.com>
2026-03-26 10:18:18 +0600

    PR requested by devendrn#79

--- tool/pack.py
+++ tool/pack.py
@@ -154,11 +154,11 @@ def run(args):
 
     patch_warning = "Only works with "
     if profile == 'android':
-        patch_warning += "Patched Minecraft"
+        patch_warning += "MB Loader"
     elif profile == 'windows':
         patch_warning += "BetterRenderDragon"
     elif profile == 'merged':
-        patch_warning += "BetterRenderDragon or Patched Minecraft"
+        patch_warning += "MB Loader (Android) or BetterRenderDragon (Windows)\nFor iOS, materials need to be installed manually for shader to work"
     else:  # ios
         patch_warning = "Materials need to be installed manually for shader to work"
 
